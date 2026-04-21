63a82a650845816933ad979cdd80d419f7a065ce
Ashwani160 <ashwani11002004@gmail.com>
2026-04-22 04:35:29 +0530

    add client url in cors

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app=express();
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.CLIENT_URL || 'http://localhost:5173',
     credentials:true
 }))
 
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -4,7 +4,7 @@ import roomHandlers from "./roomHandlers.js"
 const initSocket=(httpServer)=>{
     const io= new Server(httpServer,{
         cors:{
-            origin:'http://localhost:5173',
+            origin:process.env.CLIENT_URL || 'http://localhost:5173',
             methods:['GET', 'POST']
         }
     })
